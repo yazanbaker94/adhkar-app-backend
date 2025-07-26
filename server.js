0acc56ff94e0c4953c547180af1f369956eeea11
yazanbaker94 <79137633+yazanbaker94@users.noreply.github.com>
2025-07-26 15:57:52 +0300

    backend

--- server.js
+++ server.js
@@ -69,7 +69,6 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static('.'));
-app.use('/previews', express.static(path.join(__dirname, 'previews')));
 
 // Add request logging
 app.use((req, res, next) => {
